15204534e454764efbb173abff4b1feecdf8de19
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-10-03 10:50:23 +0200

    First commit

---
+++ src/main/java/com/example/pumpking_backend/PumpkingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pumpking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumpkingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PumpkingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pumpking_backend/PumpkingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pumpking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumpkingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
