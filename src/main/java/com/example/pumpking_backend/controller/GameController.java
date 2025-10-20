d2bd43da008ce8c7eeb441405b80deeeccc9d120
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-10-20 12:06:01 +0200

    Change endpoint to api/

--- src/main/java/com/example/pumpking_backend/controller/GameController.java
+++ src/main/java/com/example/pumpking_backend/controller/GameController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/games")
+@RequestMapping("/api/games")
 public class GameController {
 
     private final GameService gameService;
