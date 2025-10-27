9f6f474f955a0b63bd599521c1feba471cc1ba58
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-10-27 08:19:16 +0100

    Add method to get list of finished games

--- src/main/java/com/example/pumpking_backend/repository/GameRepository.java
+++ src/main/java/com/example/pumpking_backend/repository/GameRepository.java
@@ -3,8 +3,10 @@ package com.example.pumpking_backend.repository;
 import com.example.pumpking_backend.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface GameRepository extends JpaRepository <Game, Integer> {
 
+    List<Game> findByFinished(boolean isFinished);
 }
